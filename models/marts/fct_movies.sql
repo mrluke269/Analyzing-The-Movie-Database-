05eba62628f436fcd4a53896e848fabf89ca198f
Luke Mai <luke.trmai@outlook.com>
2025-10-04 19:08:14 +0000

    created dim_crews.sql and its bridge table to movies. connected dim_crews.sql to fct_movies.sql

--- models/marts/fct_movies.sql
+++ models/marts/fct_movies.sql
@@ -18,11 +18,13 @@ SELECT
     original_language,
     mg.genre_id,
     mc.company_id,
-    ma.actor_id
+    ma.actor_id,
+    mcr.crew_id
 from raw_movies as m
 left join {{ ref('movie_genre_bridge') }} as mg on m.id = mg.movie_id
 left join {{ref('movie_company_bridge')}} as mc on m.id = mc.movie_id
 left join {{ref('movie_actor_bridge')}} as ma on m.id = ma.movie_id
+left join {{ref('movie_crew_bridge')}} as mcr on m.id = mcr.movie_id
 )
 
 select * from renamed
