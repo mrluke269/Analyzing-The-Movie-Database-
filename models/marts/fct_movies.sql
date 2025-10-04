d9e249ede3e2da828a551337756e589b53a19c8c
Luke Mai <luke.trmai@outlook.com>
2025-10-04 19:44:54 +0000

    finalized fct_movies.sql

--- models/marts/fct_movies.sql
+++ models/marts/fct_movies.sql
@@ -6,7 +6,6 @@ renamed as (
 SELECT
     id as movie_id,
     title,
-    original_title,
     release_date,
     budget,
     revenue,
@@ -14,17 +13,8 @@ SELECT
     popularity,
     vote_average,
     vote_count,
-    status,
-    original_language,
-    mg.genre_id,
-    mc.company_id,
-    ma.actor_id,
-    mcr.crew_id
-from raw_movies as m
-left join {{ ref('movie_genre_bridge') }} as mg on m.id = mg.movie_id
-left join {{ref('movie_company_bridge')}} as mc on m.id = mc.movie_id
-left join {{ref('movie_actor_bridge')}} as ma on m.id = ma.movie_id
-left join {{ref('movie_crew_bridge')}} as mcr on m.id = mcr.movie_id
+    status
+from raw_movies
 )
 
 select * from renamed
