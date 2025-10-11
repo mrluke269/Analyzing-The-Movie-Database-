cf3808d00ddeec87b95c74ce8d66ed93de1431dd
Luke Mai <luke.trmai@outlook.com>
2025-10-11 22:30:15 +0000

    Treated duplicates in gender feature of model dim_actor.sql

--- models/intermediate/dimensions/dim_actor.sql
+++ models/intermediate/dimensions/dim_actor.sql
@@ -1,14 +1,25 @@
-with 
-raw_cast as 
-(select * from {{ ref('stg_movies__raw_credit') }}),
-  
-renamed as   
-  (select distinct
-    c.value['id']::string as actor_id,
-    c.value['name']::string as actor_name,
-    c.value['gender']::int as gender
-  from raw_cast rc,
-  lateral flatten(input => parse_json(rc.cast_json)) as c
+with actors_raw as (
+    select distinct
+        c.value:id::string as actor_id,
+        c.value:name::string as actor_name,
+        c.value:gender::int as gender
+    from {{ ref('stg_movies__raw_credit') }} rc,
+    lateral flatten(input => rc.cast_json) as c
+    where c.value:id is not null
+),
+
+-- Deduplicate by taking the most common gender value (or non-zero if available)
+actors_deduped as (
+    select 
+        actor_id,
+        actor_name,
+        MAX(gender) as gender
+    from actors_raw
+    group by actor_id, actor_name
 )
 
-select * from renamed
+select 
+    actor_id,
+    actor_name,
+    gender
+from actors_deduped
