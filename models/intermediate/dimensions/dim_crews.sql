ed6ef385e6eb8d8c142c3063746e34bdfa663757
Luke Mai <luke.trmai@outlook.com>
2025-10-11 05:22:09 +0000

    Fixed the dim_crews model to be true dimension

--- models/intermediate/dimensions/dim_crews.sql
+++ models/intermediate/dimensions/dim_crews.sql
@@ -4,12 +4,9 @@ raw_credit as
   
 renamed as   
   (select distinct
-    c.value['credit_id']::string as credit_id,
     c.value['id']::string as crew_id,
-    c.value['job']::string as job,
     c.value['name']::string as crew_name,
-    c.value['gender']::int as gender,
-    c.value['department']::string as department_name
+    c.value['gender']::int as gender
   from raw_credit rc,
   lateral flatten(input => parse_json(rc.crew_json)) as c
 )
