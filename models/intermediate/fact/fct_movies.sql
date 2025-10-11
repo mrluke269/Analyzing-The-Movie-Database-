4f2c429d04ee7945063c2e0cc91af6d87a30ab2d
Luke Mai <luke.trmai@outlook.com>
2025-10-11 04:07:50 +0000

    added handle nulls for fct_movies.sql model

--- models/intermediate/fact/fct_movies.sql
+++ models/intermediate/fact/fct_movies.sql
@@ -3,18 +3,18 @@ with raw_movies as(
 ),
 
 renamed as (
-SELECT
-    id as movie_id,
-    title,
-    release_date,
-    budget,
-    revenue,
-    runtime,
-    popularity,
-    vote_average,
-    vote_count,
-    status
-from raw_movies
+    SELECT
+        id as movie_id,
+        title,
+        release_date,
+        COALESCE(budget, 0) as budget,  -- Convert nulls to 0
+        COALESCE(revenue, 0) as revenue,  -- Convert nulls to 0
+        runtime,  -- OK to be null for unreleased movies
+        COALESCE(popularity, 0) as popularity,
+        COALESCE(vote_average, 0) as vote_average,
+        COALESCE(vote_count, 0) as vote_count,
+        status
+    from raw_movies
 )
 
-select * from renamed
+select * from renamed;
