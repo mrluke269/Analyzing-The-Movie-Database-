e088e613247f17626ac4da0599e30d78736b313f
Luke Mai <luke.trmai@outlook.com>
2025-10-11 04:10:54 +0000

    changed back to allow null to avoid skew when aggregated

--- models/intermediate/fact/fct_movies.sql
+++ models/intermediate/fact/fct_movies.sql
@@ -19,18 +19,4 @@ from raw_movies
 
 select * from renamed;
 
--- In your fct_movies.sql
-renamed as (
-    SELECT
-        id as movie_id,
-        title,
-        release_date,
-        COALESCE(budget, 0) as budget,  -- Convert nulls to 0
-        COALESCE(revenue, 0) as revenue,  -- Convert nulls to 0
-        runtime,  -- OK to be null for unreleased movies
-        COALESCE(popularity, 0) as popularity,
-        COALESCE(vote_average, 0) as vote_average,
-        COALESCE(vote_count, 0) as vote_count,
-        status
-    from raw_movies
-)
+
