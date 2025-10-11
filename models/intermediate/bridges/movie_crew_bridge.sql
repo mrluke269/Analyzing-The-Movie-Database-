261ec7fb7be1f6ff8cf8a01edb53525eb82b9766
Luke Mai <luke.trmai@outlook.com>
2025-10-11 05:23:29 +0000

    Fixed movie_crew bridge to include key details

--- models/intermediate/bridges/movie_crew_bridge.sql
+++ models/intermediate/bridges/movie_crew_bridge.sql
@@ -1,5 +1,8 @@
 SELECT
   c.movie_id::string AS movie_id,
-  cj.VALUE:"id"::string AS crew_id
+  cj.VALUE:"id"::string AS crew_id,
+  cj.VALUE:"credit_id"::string as credit_id,
+  cj.VALUE:"job"::string as job,
+  cj.VALUE:"department"::string as department_name
 FROM {{ ref('stg_movies__raw_credit') }} AS c,
 LATERAL FLATTEN(INPUT => PARSE_JSON(c.crew_json)) AS cj
